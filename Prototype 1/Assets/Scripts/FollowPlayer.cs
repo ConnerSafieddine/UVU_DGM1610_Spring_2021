e875a9cc512882320f4bdda838f38074de5f37cb
Sayed Conner <10846002@my.uvu.edu>
2021-01-25 17:10:11 -0700

    Added Player side and forward movement

--- Prototype 1/Assets/Scripts/FollowPlayer.cs
+++ Prototype 1/Assets/Scripts/FollowPlayer.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class FollowPlayer : MonoBehaviour
 {
     public GameObject player;
-
+    private Vector3 Offset = new Vector3(0, 5, -7);
     // Start is called before the first frame update
     void Start()
     {
@@ -15,6 +15,7 @@ public class FollowPlayer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position = player.transform.position;
+        transform.position = player.transform.position + Offset;
+
     }
 }
--- Prototype 1/Assets/Scripts/PlayerController.cs
+++ Prototype 1/Assets/Scripts/PlayerController.cs
@@ -1,11 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+//these are libraries full of commands used to talk to Unity
 public class PlayerController : MonoBehaviour
 {
     public float speed = 5.0f;
-
+    public float hInput;
+    public float vInput;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,7 +16,9 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // Move the vehicle forward
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        hInput = Input.GetAxis("Horizontal");
+        vInput = Input.GetAxis("Vertical");
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * vInput);
+        transform.Translate(Vector3.right * Time.deltaTime * speed * hInput);
     }
 }
