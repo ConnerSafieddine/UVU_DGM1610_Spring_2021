e89bf7daa9ec16e946321e6af40b60b892e77987
Sayed Conner <10846002@my.uvu.edu>
2021-01-26 20:42:19 -0700

    Player translates and rotates with camera follow offset

--- Prototype 1/Assets/Scripts/PlayerController.cs
+++ Prototype 1/Assets/Scripts/PlayerController.cs
@@ -4,9 +4,11 @@ using UnityEngine;
 //these are libraries full of commands used to talk to Unity
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 5.0f;
-    public float hInput;
-    public float vInput;
+    // Private variables
+    private float speed = 10.0f;
+    private float turnSpeed = 25;
+    private float horizontalInput;
+    private float forwardInput;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,9 +18,13 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        hInput = Input.GetAxis("Horizontal");
-        vInput = Input.GetAxis("Vertical");
-        transform.Translate(Vector3.forward * Time.deltaTime * speed * vInput);
-        transform.Translate(Vector3.right * Time.deltaTime * speed * hInput);
+        // This is where we get player input
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+
+        // We move the vehicle forward
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        // We turn the vehicle
+        transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
     }
 }
