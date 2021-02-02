15b5e3c919372f0e4f6edf6ca22349fe01d9d365
Sayed Conner <10846002@my.uvu.edu>
2021-02-01 17:20:34 -0700

    Player Moves left and right and has a limit for how far

---
+++ Prototype 2/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private float horizontalInput;
+    private float speed = 10.0f;
+    private float xRange = 10.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // If the player moves left 10 it will prevent the player to move left more
+        if (transform.position.x < -xRange)
+        {
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        }
+        // If the player moves right 10 it will prevent the player to move right more
+        if (transform.position.x > xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+        // Getting player input A & D
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+
+    }
+}
