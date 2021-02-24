e56832478492bf22dc328a5ef949eb6424b8e911
Sayed Conner <10846002@my.uvu.edu>
2021-02-24 14:10:07 -0700

    Lesson 3.2 Completed

---
+++ Prototype 3/Assets/Scripts/RepeatBackground.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RepeatBackground : MonoBehaviour
+{
+    private Vector3 startPos;
+    public float repeatWidth = 112.8f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Divided the width of the background on x axis by 2
+        startPos = transform.position;
+        repeatWidth = GetComponent<BoxCollider>().size.x / 2;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Repeating background on x axis
+        if (transform.position.x < startPos.x - repeatWidth)
+        {
+            transform.position = startPos;
+        }
+
+    }
+}
