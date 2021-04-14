0eb03c68e8e4490305b2065f53da55091f770524
Sayed Conner <10846002@my.uvu.edu>
2021-04-13 21:14:58 -0600

    5.1 & 5.2 Completed

---
+++ Prototype 5/Assets/Scripts/Target.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+    private Rigidbody targetRb;
+    private GameManager gameManager;
+    private float minSpeed = 12;
+    private float maxSpeed = 16;
+    private float maxTorque = 10;
+    private float xRange = 4;
+    private float ySpawnPos = 3.5f;
+
+    public ParticleSystem explosionParticle;
+    public int pointValue;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Getting the rigidbody component 
+        targetRb = GetComponent<Rigidbody>();
+        // Finding the game manager script
+        gameManager = GameObject.Find("Game Manager").GetComponent<GameManager>();
+
+        //
+        targetRb.AddForce(RandomForce(), ForceMode.Impulse);
+        targetRb.AddTorque(RandomTorque(), RandomTorque(), RandomTorque(), ForceMode.Impulse);
+
+        transform.position = RandomSpawnPos();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseDown()
+    {
+        Destroy(gameObject);
+        gameManager.UpdateScore(pointValue);
+        Instantiate(explosionParticle, transform.position, transform.rotation);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(gameObject);
+    }
+
+    Vector2 RandomForce()
+    {
+        return Vector2.up * Random.Range(minSpeed, maxSpeed);
+    }
+
+    float RandomTorque()
+    {
+        return Random.Range(-maxTorque, maxTorque);
+    }
+
+    Vector2 RandomSpawnPos()
+    {
+        return new Vector2(Random.Range(-xRange, xRange), -ySpawnPos);
+    }
+
+    private float DoubleNumber(float number)
+    {
+        return number *= 2;
+    }
+}
